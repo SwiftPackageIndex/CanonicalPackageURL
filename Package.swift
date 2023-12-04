f8e9616c42d5ef63f6bdac1b40b4f1d7966db6e6
Sven A. Schmidt <sas@finestructure.co>
2023-12-04 13:00:07 +0100

    Add swift-parsing

--- Package.swift
+++ Package.swift
@@ -4,16 +4,22 @@ import PackageDescription
 
 let package = Package(
     name: "CanonicalPackageURLs",
+    platforms: [.macOS(.v10_15)],
     products: [
         .library(
             name: "CanonicalPackageURLs",
             targets: ["CanonicalPackageURLs"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/pointfreeco/swift-parsing", revision: "0.12.0")
+    ],
     targets: [
         .target(
-            name: "CanonicalPackageURLs"),
-        .testTarget(
-            name: "CanonicalPackageURLsTests",
-            dependencies: ["CanonicalPackageURLs"]),
+            name: "CanonicalPackageURLs",
+            dependencies: [
+                .product(name: "Parsing", package: "swift-parsing")
+            ]
+        ),
+        .testTarget(name: "CanonicalPackageURLsTests", dependencies: ["CanonicalPackageURLs"]),
     ]
 )
