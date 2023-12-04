5abe292debfa27d6de75572a5507c1b069d9e05e
Sven A. Schmidt <sas@finestructure.co>
2023-12-04 13:23:52 +0100

    Add more tests

--- Tests/CanonicalPackageURLsTests/CanonicalPackageURLsTests.swift
+++ Tests/CanonicalPackageURLsTests/CanonicalPackageURLsTests.swift
@@ -1,7 +1,29 @@
 import XCTest
 @testable import CanonicalPackageURLs
 
+
+typealias Prefix = CanonicalPackageURL.Prefix
+
 final class CanonicalPackageURLsTests: XCTestCase {
+
+    func test_Prefix_parsing() throws {
+        XCTAssertEqual(try Prefix.parser(of: .gitAt).parse("git@"), .gitAt)
+        XCTAssertEqual(try Prefix.parser(of: .gitAt).parse("GIT@"), .gitAt)
+        XCTAssertEqual(try Prefix.parser(of: .gitAt).parse("Git@"), .gitAt)
+        XCTAssertThrowsError(try Prefix.parser(of: .gitAt).parse("git"))
+    }
+
+    func test_Prefix_description() throws {
+        XCTAssertEqual(Prefix.gitAt.description, "git@")
+        XCTAssertEqual(Prefix.http.description, "http")
+        XCTAssertEqual(Prefix.https.description, "https")
+    }
+
+    func test_Prefix_matcher() throws {
+        XCTAssertEqual(Prefix.gitAt.matcher, "git@")
+        XCTAssertEqual(Prefix.http.matcher, "http://")
+        XCTAssertEqual(Prefix.https.matcher, "https://")
+    }
 
     func test_parser() throws {
         XCTAssertEqual(try CanonicalPackageURL.parser.parse("git@github.com:andtie/SequenceBuilder.git"),
