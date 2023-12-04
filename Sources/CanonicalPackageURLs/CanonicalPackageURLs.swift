ed3c6b6931a74a1c9e00b75dd5d81bb128499d5e
Sven A. Schmidt <sas@finestructure.co>
2023-12-04 13:17:48 +0100

    Add basic parser

--- Sources/CanonicalPackageURLs/CanonicalPackageURLs.swift
+++ Sources/CanonicalPackageURLs/CanonicalPackageURLs.swift
@@ -1,2 +1,63 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Parsing
+
+
+struct CanonicalPackageURL: Equatable {
+    var prefix: Prefix
+    var hostname: String
+    var path: String
+
+    enum Prefix: String, CustomStringConvertible {
+        case gitAt = "git@"
+        case http = "http"
+        case https = "https"
+
+        static func parser(of prefix: Prefix) -> some Parser<Substring, Prefix> {
+            Parse {
+                PrefixUpTo(prefix.matcher) { $0.lowercased() == $1.lowercased() }
+                StartsWith(prefix.matcher) { $0.lowercased() == $1.lowercased() }
+            }
+            .map { _ in prefix }
+        }
+        var description: String { rawValue }
+        var matcher: String {
+            switch self {
+                case .gitAt: rawValue
+                case .http: rawValue + "://"
+                case .https: rawValue + "://"
+            }
+        }
+    }
+
+    static var hostname: some Parser<Substring, String> {
+        Parse {
+            OneOf {
+                PrefixUpTo(":")
+                PrefixUpTo("/")
+            }.map(.string)
+            Skip { OneOf { ":"; "/" } }
+        }
+    }
+
+    static var suffix: some Parser<Substring, String> {
+        OneOf {
+            Parse {
+                PrefixUpTo(".git") { $0.lowercased() == $1.lowercased() }
+                StartsWith(".git") { $0.lowercased() == $1.lowercased() }
+            }
+            Rest()
+        }.map(.string)
+    }
+
+    static var parser: some Parser<Substring, Self> {
+        Parse {
+            OneOf {
+                Prefix.parser(of: .gitAt)
+                Prefix.parser(of: .http)
+                Prefix.parser(of: .https)
+            }
+            hostname
+            suffix
+        }
+        .map(Self.init(prefix:hostname:path:))
+    }
+}
--- Tests/CanonicalPackageURLsTests/CanonicalPackageURLsTests.swift
+++ Tests/CanonicalPackageURLsTests/CanonicalPackageURLsTests.swift
@@ -2,11 +2,14 @@ import XCTest
 @testable import CanonicalPackageURLs
 
 final class CanonicalPackageURLsTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
 
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    func test_parser() throws {
+        XCTAssertEqual(try CanonicalPackageURL.parser.parse("git@github.com:andtie/SequenceBuilder.git"),
+                       .init(prefix: .gitAt, hostname: "github.com", path: "andtie/SequenceBuilder"))
+        XCTAssertEqual(try CanonicalPackageURL.parser.parse("http://github.com/apple/swift-argument-parser"),
+                       .init(prefix: .http, hostname: "github.com", path: "apple/swift-argument-parser"))
+        XCTAssertEqual(try CanonicalPackageURL.parser.parse("https://gitee.com/zexu007/Kingfisher.git"),
+                       .init(prefix: .https, hostname: "gitee.com", path: "zexu007/Kingfisher"))
     }
+
 }
