14eb86017e67c0e359e6a076f7de5c68cd875739
Sven A. Schmidt <sas@finestructure.co>
2023-12-08 14:13:43 +0100

    Unpin swift-parsing

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["CanonicalPackageURLs"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/pointfreeco/swift-parsing", revision: "0.12.0")
+        .package(url: "https://github.com/pointfreeco/swift-parsing.git", from: "0.12.0")
     ],
     targets: [
         .target(
